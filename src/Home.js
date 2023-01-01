6286ea53c80ad52e21fb19487966b141fc270e1a
Arvind <arvind.kumar@hoogaa.com>
2023-01-01 08:00:10 +0530

    update

---
+++ src/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Common from './Common'
+import img3 from './images/img4.jpg'
+const Home = () => {
+  return (
+    <>
+
+
+      <Common
+        imgsrc4={img3}
+        name='Grow your Business With'
+        btn='Get Started'
+        visit='./service'
+      />
+    </>
+  )
+}
+
+export default Home
